9b3a7abb7d106da4b4c9a26a8f1054088f9b859f
Pablo <54485302+thpablo@users.noreply.github.com>
2023-11-03 20:34:51 -0300

    Add files via upload

---
+++ fileGenerator.h
@@ -0,0 +1,20 @@
+#ifndef FILE_GENERATOR
+#define FILE_GENERATOR
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#define LEN 80
+
+typedef struct
+{
+    int key;
+    long int longNumber;
+    char randChars[LEN];
+} Data;
+
+void randomString(char *s, const int len, int timex);
+void generatorBinFile(int n, int op);
+FILE *openFile(const char *nomeArquivo, const char *modo);
+void shuffle(Data arr[], int n);
+#endif 
